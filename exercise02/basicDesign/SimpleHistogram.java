45dc1f2e7e4149eb1475a4ef935adbc91ced458c
ecly <ecly@mailbox.org>
2017-09-08 11:47:48 +0200

    almost finished exercise 2.

--- exercise02/basicDesign/SimpleHistogram.java
+++ exercise02/basicDesign/SimpleHistogram.java
@@ -1,43 +1,124 @@
 // For week 2
 // sestoft@itu.dk * 2014-09-04
+import java.util.concurrent.atomic.*;
 
 class SimpleHistogram {
-  public static void main(String[] args) {
-    final Histogram histogram = new Histogram1(30);
-    histogram.increment(7);
-    histogram.increment(13);
-    histogram.increment(7);
-    dump(histogram);
-  }
+    public static void main(String[] args) {
+        final int range = 5_000_000;
+        final Histogram4 histogram = new Histogram4(range);
 
-  public static void dump(Histogram histogram) {
-    int totalCount = 0;
-    for (int bin=0; bin<histogram.getSpan(); bin++) {
-      System.out.printf("%4d: %9d%n", bin, histogram.getCount(bin));
-      totalCount += histogram.getCount(bin);
+        Thread[] threads = new Thread[10];
+        for(int i = 0; i < 10; i++){
+            final int h = i;
+            threads[i]=  new Thread(() -> { 
+                for (int p=h*499_999+h; p<(h+1)*499_999+h+1; p++)
+                    histogram.increment(countFactors(p));
+            });
+        }
+
+        for(Thread thread : threads){
+            thread.start();
+        }
+
+        for(Thread thread : threads){
+            try{ thread.join();}
+            catch(Exception e){System.out.println(e);}
+        }
+
+        for(int i = 0; i < 10; i++){
+            System.out.printf("%d: %d \n", i, histogram.getCount(i));
+        }
     }
-    System.out.printf("      %9d%n", totalCount);
-  }
+    public static int countFactors(int p) {
+        if (p < 2)
+            return 0;
+        int factorCount = 1, k = 2;
+        while (p >= k * k) {
+            if (p % k == 0) {
+                factorCount++;
+                p /= k;
+            } else 
+                k++;
+        }
+        return factorCount;
+    }
+    public static void dump(Histogram histogram) {
+        int totalCount = 0;
+        for (int bin=0; bin<histogram.getSpan(); bin++) {
+            System.out.printf("%4d: %9d%n", bin, histogram.getCount(bin));
+            totalCount += histogram.getCount(bin);
+        }
+        System.out.printf("      %9d%n", totalCount);
+    }
 }
 
 interface Histogram {
-  public void increment(int bin);
-  public int getCount(int bin);
-  public int getSpan();
+    public void increment(int bin);
+    public int getCount(int bin);
+    public int getSpan();
+}
+
+class Histogram4 implements Histogram {
+    private AtomicIntegerArray counts;
+    public Histogram4(int span) {
+        this.counts = new AtomicIntegerArray(span);
+    }
+    public void increment(int bin) {
+        counts.getAndAdd(bin, 1); 
+    }
+    public int getCount(int bin) {
+        return counts.get(bin);
+    }
+    public int getSpan() {
+        return counts.length();
+    }
+}
+class Histogram3 implements Histogram {
+    private AtomicInteger[] counts;
+    public Histogram3(int span) {
+        this.counts = new AtomicInteger[span];
+        for(int i = 0; i < span; i++)
+            counts[i] = new AtomicInteger(0);
+    }
+    public void increment(int bin) {
+        counts[bin].incrementAndGet();
+    }
+    public int getCount(int bin) {
+        return counts[bin].get();
+    }
+    public int getSpan() {
+        return counts.length;
+    }
+}
+
+class Histogram2 implements Histogram {
+    private int[] counts;
+    public Histogram2(int span) {
+        this.counts = new int[span];
+    }
+    public synchronized void increment(int bin) {
+        counts[bin] = counts[bin] + 1;
+    }
+    public synchronized int getCount(int bin) {
+        return counts[bin];
+    }
+    public int getSpan() {
+        return counts.length;
+    }
 }
 
 class Histogram1 implements Histogram {
-  private int[] counts;
-  public Histogram1(int span) {
-    this.counts = new int[span];
-  }
-  public void increment(int bin) {
-    counts[bin] = counts[bin] + 1;
-  }
-  public int getCount(int bin) {
-    return counts[bin];
-  }
-  public int getSpan() {
-    return counts.length;
-  }
+    private int[] counts;
+    public Histogram1(int span) {
+        this.counts = new int[span];
+    }
+    public void increment(int bin) {
+        counts[bin] = counts[bin] + 1;
+    }
+    public int getCount(int bin) {
+        return counts[bin];
+    }
+    public int getSpan() {
+        return counts.length;
+    }
 }
